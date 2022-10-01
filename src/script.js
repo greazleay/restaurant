70d81e03e1501bf81b08a5b19a892a0840b927b0
Lekan Adetunmbi <lothbroch@gmail.com>
2022-10-01 12:34:25 +0100

    Added external API call and improved styling

--- src/script.js
+++ src/script.js
@@ -2,10 +2,42 @@ import sandwich from './asset/sandwich.png';
 import hamburger from './asset/hamburger.png';
 import bibimbub from './asset/bibimbub.png';
 
-export function switchToHome() {
+function getMain() {
     const main = document.querySelector('main');
     main.childNodes.forEach(child => main.removeChild(child));
-    
+    return main
+}
+
+function menuItem(url, name, desc, price, parent) {
+    let article = document.createElement('article');
+
+    let img = document.createElement('img');
+    img.src = url;
+
+    let div = document.createElement('div')
+
+    let h1 = document.createElement('h1');
+    h1.textContent = name;
+
+    let p = document.createElement('p');
+    p.textContent = desc;
+
+    let span = document.createElement('span');
+    span.textContent = '$' + price;
+
+    div.appendChild(h1);
+    div.appendChild(p);
+    div.appendChild(span);
+
+    article.appendChild(img);
+    article.appendChild(div);
+
+    parent.appendChild(article)
+}
+
+export function switchToHome() {
+    const main = getMain()
+
     const home = document.createElement('section');
     home.classList.add('home');
     main.appendChild(home)
@@ -20,37 +52,37 @@ export function switchToHome() {
     home.appendChild(button)
 }
 
-export function switchToMenu() {
-    const main = document.querySelector('main');
-    main.childNodes.forEach(child => main.removeChild(child));
-    
+export async function switchToMenu() {
+    const main = getMain()
+
     const menu = document.createElement('section');
     menu.classList.add('menu');
     main.appendChild(menu)
 
-    function menuEntry(url, name) {
-        let article = document.createElement('article');
-        let img = document.createElement('img');
-        img.src = url;
-        let h1 = document.createElement('h1');
-        h1.textContent = name;
-        let p = document.createElement('p');
-        p.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
-        article.appendChild(img);
-        article.appendChild(h1);
-        article.appendChild(p);
-        menu.appendChild(article)
-    }
+    const res = await fetch('https://ig-food-menus.herokuapp.com/burgers')
+    const data = await res.json();
 
-    menuEntry(sandwich, 'Sandwich');
-    menuEntry(hamburger, 'Hamburger');
-    menuEntry(bibimbub, 'Bibimbub')
+    if (data.length) {
+
+        data.forEach(item => {
+            const { img, name, dsc, price } = item;
+            menuItem(img, name.trim(), dsc.trim(), price, menu);
+        })
+
+    } else {
+
+        const desc = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.';
+
+        menuItem(sandwich, 'Sandwich', desc, 50, menu);
+        menuItem(hamburger, 'Hamburger', desc, 36, menu);
+        menuItem(bibimbub, 'Bibimbub', desc, 97, menu)
+
+    }
 }
 
 export function switchToContact() {
-    const main = document.querySelector('main');
-    main.childNodes.forEach(child => main.removeChild(child));
-    
+    const main = getMain()
+
     const contact = document.createElement('section');
     contact.classList.add('contact');
     main.appendChild(contact)
